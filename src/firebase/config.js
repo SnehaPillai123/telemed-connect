6b131cd4fea09f16c1242295fad53d9e65e047eb
SnehaPillai123 <sneha24comp@student.mes.ac.in>
2026-04-05 01:23:47 +0530

    Fix duplicate getStorage import

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,4 +1,3 @@
-import { getStorage } from "firebase/storage";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
@@ -18,5 +17,4 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
 export const db = getFirestore(app);
-export const storage = getStorage(app);
 export default app;
